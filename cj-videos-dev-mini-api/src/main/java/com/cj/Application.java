fd1e00ed6cbe29544e458b4ea6c51accc05b0331
cuijia <492309801@qq.com>
2019-05-24 17:20:06 +0800

    创建仿抖音接口后台

---
+++ cj-videos-dev-mini-api/src/main/java/com/cj/Application.java
@@ -0,0 +1,16 @@
+package com.cj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages="com.cj")
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
---
+++ cj-videos-dev-mini-api/src/main/java/com/cj/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.cj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public String test() {
+		return "hello spring boot~";
+	}
+}
